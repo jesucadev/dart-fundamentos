56928004ea058f6c41f1f051e9878d3a15eb4be1
Jesús Mendoza <jesusdlc.m15@gmail.com>
2023-09-08 10:47:16 -0600

    Creación de archivos y 2do ejercicio realizado

---
+++ mapas.dart
@@ -0,0 +1,27 @@
+void main() {
+  
+  final String lion = 'Corzo';
+  final int hp = 100;
+  final bool isAlive = true;
+  final List<String> abilities = ['enemie'];
+  final sprites = <String>['Corzo/frente.png','Corzo/deespalda.png'];
+  
+// dynamic == null
+  dynamic errorMessage = 'Hola';
+  errorMessage = true;
+  errorMessage = [1,2,3,4,5,6];
+  errorMessage = { 1,2,3,4,5,6 };
+  errorMessage = () => true;
+  errorMessage = null;
+    
+  print("""
+  $lion
+  $hp
+  $isAlive
+  $abilities
+  $sprites
+  $errorMessage
+  """);
+  
+  
+}
